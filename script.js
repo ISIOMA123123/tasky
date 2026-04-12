5a57707988c9ab957ffba8eccccab8a5f42ed8bb
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-12 19:42:04 +0100

    Create script.js
    created js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const checkbox = document.getElementById("checkbox");
+
+const title = document.querySelector("[data-testid='test-todo-title']");
+
+const status = document.querySelector("[data-testid='test-todo-status']");
