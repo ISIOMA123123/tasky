737bd4212441c89faaaacf58608ce7e2401f7329
ISIOMA123123 <ubanisuccess26@gmail.com>
2026-04-13 13:15:59 +0100

    Update script.js
    added some javascript

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ const checkbox = document.getElementById("checkbox");
 const title = document.querySelector("[data-testid='test-todo-title']");
 
 const status = document.querySelector("[data-testid='test-todo-status']");
+
+checkbox.addEventListener("change", function name(params) {
+    if (checkbox.checked) {
+        title.style.textDecoration = "line-through";
+        status.textContent = "Done";
+    } else {
+        title.style.textDecoration = "none";
+        status.textContent = "Pending";
+    }
+});
+
+const dueDateElement = document.getElementById("dueDate");
+
+const timeRemainingElement = document.getElementById
